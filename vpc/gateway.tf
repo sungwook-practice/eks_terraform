d9883677970c9633e3e2ec300611fe4ede1aec46
choisungwook <kgg1959@naver.com>
2022-12-20 20:47:46 +0900

    feature: nat gateway 추가

--- vpc/gateway.tf
+++ vpc/gateway.tf
@@ -5,3 +5,24 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.cluster_name}"
   }
 }
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  lifecycle {
+    # prevent_destroy = true
+  }
+
+  tags = {
+    Name = "${var.cluster_name}"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public[element(keys(aws_subnet.public), 0)].id
+
+  tags = {
+    Name        = "${var.cluster_name}-ngw"
+  }
+}
--- vpc/route_tables.tf
+++ vpc/route_tables.tf
@@ -19,22 +19,23 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# resource "aws_route_table" "private" {
-#   vpc_id = aws_vpc.eks.id
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.eks.id
 
-#   tags = {
-#     Name = "${var.cluster_name}-private"
-#   }
-# }
+  tags = {
+    Name = "${var.cluster_name}-private"
+  }
+}
 
-# resource "aws_route" "private" {
-#   route_table_id         = aws_route_table.private.id
-#   destination_cidr_block = var.vpc_cidr
-# }
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
+}
 
-# resource "aws_route_table_association" "private" {
-#   for_each = aws_subnet.private
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
 
-#   subnet_id      = aws_subnet.private[each.key].id
-#   route_table_id = aws_route_table.private.id
-# }
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private.id
+}
